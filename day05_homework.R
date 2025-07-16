28b2796a35e732a9df9ab45e1f527eae481e91c2
next-level-yu <24zb1009@student.gs.chiba-u.jp>
2025-07-16 21:17:37 +0900

    Finish Homework

--- day05_homework.R
+++ day05_homework.R
@@ -1,6 +1,6 @@
 # DAY 5 HOMEWORK
 #
-# Name:
+# Name:Yuma Sato
 
 # NOTE: In the text below, backticks ``
 # are used to indicate code. For example, `library(tidyverse)`
@@ -16,16 +16,27 @@
 #
 # HINT: the `tailnum` column uniquely identifies each airplane.
 
+answer_1 <- 
+  flights |>
+  left_join(planes)
+
 # Q2. Use answer_1 to count the number of flights made by each manufacturer,
 # then sort the results so the manufacturer with the most flights is at the top.
 # Save the result as answer_2
 #
 # HINT: use `count()`
 
+answer_2 <-
+  answer_1 |>
+  count(manufacturer, sort = TRUE)
+
 # Q3. Does every flight in the `flights` dataframe have a corresponding
 # airplane in `planes`?
 #
 # Save your answer as a logical vector (either TRUE or FALSE) to answer_3
+
+answer_3 <- FALSE
+  
 
 # Q4. The `nycflights13` package includes a dataframe called `airlines` that
 # includes the name of each airline and its corresponding carrier code.
@@ -33,6 +44,10 @@
 #
 # Join the `airlines` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_4
+
+answer_4 <- 
+  airlines |>
+  left_join(flights)
 
 # Q5. Use answer_4 to find the average arrival delay for each airline.
 # Exclude missing values from your calculation.
@@ -44,3 +59,7 @@
 # HINT: when you use functions like `mean()`, `max()`, or `min()`, use the
 # option `na.rm = TRUE` to exclude missing values from the calculation
 
+answer_5 <-
+  answer_4 |>
+  group_by(carrier) |>
+  summarise(ave_arr_delay = mean(arr_delay, na.rm = TRUE))
